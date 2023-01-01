6d87d06b6a7ff79896c573dc6c98c56c0035c851
sahidcse15@gmail.com <sahidcse15@gmail.com>
2023-01-01 17:16:34 +0530

    Initial Commit

---
+++ SahidProfileView/SahidProfileViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahidProfileViewApp.swift
+//  SahidProfileView
+//
+//  Created by Sahid Reza on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SahidProfileViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
